335b9e587d19914ef6c661547439aaf7b07e5507
tsm <attila.egyed.91@gmail.com>
2016-08-13 15:25:29 +0200

    fix code splitting

--- src/app/modules/404-page.module.ts
+++ src/app/modules/404-page.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { _404PageRoutes } from '../routes';
 import { _404Page} from '../pages';
+import { _404PageRoutes } from '../routes';
 
 @NgModule({
   declarations: [_404Page],
--- src/app/modules/index.ts
+++ src/app/modules/index.ts
@@ -1,16 +1,15 @@
+/**
+ * Do not import page modules, because of code splitting.
+ */
 import { CoreModule } from './core.module';
 import { GUIModule } from './gui.module';
 import { DataModule } from './data.module';
-import { HomePageModule } from './home-page.module';
-import { _404PageModule } from './404-page.module';
 // $1
 
 export {
   CoreModule,
   GUIModule,
   DataModule,
-  HomePageModule,
-  _404PageModule,
 // $2
 }
 
@@ -18,7 +17,5 @@ export const modules = [
   CoreModule,
   GUIModule,
   DataModule,
-  HomePageModule,
-  _404PageModule,
   // $3
 ];
